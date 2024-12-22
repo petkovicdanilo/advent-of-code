a40d713dd66431de89d30b485bc95d6522e0d8e3
Danilo Petkovic <petkovicdanilo97@gmail.com>
2024-12-22 18:12:47 +0100

    Optimize day 22 - 2024

--- 2024/src/day22.zig
+++ 2024/src/day22.zig
@@ -61,88 +61,61 @@ pub fn solvePartTwo() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var input = try getInput(allocator, "examples/day22.txt");
+    var input = try getInput(allocator, "examples/day22-2.txt");
     defer input.deinit();
 
-    var diff_list = ArrayList(ArrayList(i8)).init(allocator);
-    for (input.items) |_| {
-        try diff_list.append(ArrayList(i8).init(allocator));
-    }
-
-    var max_price = ArrayList(AutoHashMap([4]i8, u8)).init(allocator);
-    for (input.items) |_| {
-        try max_price.append(AutoHashMap([4]i8, u8).init(allocator));
-    }
+    var max_bananas = AutoHashMap([4]i8, u32).init(allocator);
+    defer max_bananas.deinit();
 
     const steps: u32 = 2000;
 
-    for (0.., input.items) |i, *number| {
+    for (input.items) |*number| {
+        var diff_list = ArrayList(i8).init(allocator);
+        defer diff_list.deinit();
+
+        var sequences_found = AutoHashMap([4]i8, void).init(allocator);
+        defer sequences_found.deinit();
+
         for (0..steps) |_| {
             const nextNumber = nextSecretNumber(number.*);
 
             const diff: i8 = @as(i8, @intCast((nextNumber % 10))) - 
                             @as(i8, @intCast(number.* % 10));
 
-            if (diff_list.items[i].items.len == 4) {
-                _ = diff_list.items[i].orderedRemove(0);
+            if (diff_list.items.len == 4) {
+                _ = diff_list.orderedRemove(0);
             }
-            try diff_list.items[i].append(diff);
+            try diff_list.append(diff);
 
             number.* = nextNumber;
 
-            if (diff_list.items[i].items.len != 4) {
-                continue;
-            }
-
-            if (max_price.items[i].contains(diff_list.items[i].items[0..4].*)) {
+            if (diff_list.items.len != 4) {
                 continue;
             }
 
             var seq: [4]i8 = undefined;
-            for (0..4) |j| {
-                seq[j] = diff_list.items[i].items[j];
+            for (0..4) |i| {
+                seq[i] = diff_list.items[i];
             }
 
-            try max_price.items[i].put(seq, @as(u8, @intCast(nextNumber % 10)));
+            if (sequences_found.contains(seq)) {
+                continue;
+            }
+
+            try sequences_found.put(seq, {});
+
+            const curr = max_bananas.get(seq) orelse 0;
+            try max_bananas.put(seq, curr + @as(u32, @intCast(nextNumber % 10)));
         }
     }
 
-    var res: u64 = 0;
-    for (0..19) |n1| {
-        const num1: i8 = @as(i8, @intCast(n1)) - 9;
-        for (0..19) |n2| {
-            const num2: i8 = @as(i8, @intCast(n2)) - 9;
-            for (0..19) |n3| {
-                const num3: i8 = @as(i8, @intCast(n3)) - 9;
-                for (0..19) |n4| {
-                    const num4: i8 = @as(i8, @intCast(n4)) - 9;
-
-                    var curr_res: u64 = 0;
-                    for (max_price.items) |mp| {
-                        if (!mp.contains(.{num1, num2, num3, num4})) {
-                            continue;
-                        }
-
-                        curr_res += mp.get(.{num1, num2, num3, num4}).?;
-                    }
-
-                    res = @max(res, curr_res);
-                }
-            }
-        }
+    var res: u32 = 0;
+    var it = max_bananas.iterator();
+    while (it.next()) |kv| {
+        res = @max(res, kv.value_ptr.*);
     }
 
     print("{d}\n", .{res});
-
-    for (diff_list.items) |diff| {
-        diff.deinit();
-    }
-    diff_list.deinit();
-
-    for (max_price.items) |*max_p| {
-        max_p.deinit();
-    }
-    max_price.deinit();
 }
 
 fn nextSecretNumber(num: u64) u64 {
