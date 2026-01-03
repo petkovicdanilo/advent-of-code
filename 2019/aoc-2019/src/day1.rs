aecde6f6bfbca23abe6fbcf7dabc55610976a9d9
Danilo Petkovic <petkovicdanilo97@gmail.com>
2026-01-03 11:35:04 +0100

    Add day 1 - 2019

--- 2019/aoc-2019/src/day1.rs
+++ 2019/aoc-2019/src/day1.rs
@@ -6,20 +6,50 @@ pub(crate) struct Day1;
 
 impl Day for Day1 {
     fn part1(&mut self, input_file: String) -> Result<(), String> {
-        load_input(input_file)?;
-        todo!()
+        let numbers = parse_input(input_file)?;
+
+        let mut res = 0;
+        for number in numbers.iter() {
+            res += get_fuel(*number);
+        }
+
+        println!("{res}");
+        return Ok(());
     }
 
-    fn part2(&mut self, _input_file: String) -> Result<(), String> {
-        todo!()
+    fn part2(&mut self, input_file: String) -> Result<(), String> {
+        let numbers = parse_input(input_file)?;
+
+        let mut res = 0;
+        for number in numbers.iter() {
+            let mut n = *number;
+            loop {
+                let fuel = get_fuel(n);
+                if fuel < 0 {
+                    break;
+                }
+                res += fuel;
+                n = fuel;
+            }
+        }
+
+        println!("{res}");
+        return Ok(());
     }
 }
 
-fn load_input(input_file: String) -> Result<(), String> {
-    println!("Loading from file {input_file}");
+fn parse_input(input_file: String) -> Result<Vec<i32>, String> {
     let contents = fs::read_to_string(input_file)
         .map_err(|e| format!("Couldn't read from file: {e}"))?;
-    println!("Contents: {contents}");
 
-    return Ok(());
+    let numbers: Result<Vec<_>, _> = contents.lines()
+        .map(|line| line.parse::<i32>())
+        .collect();
+    let numbers = numbers.map_err(|e| format!("Failed to parse input file: {e}"))?;
+
+    return Ok(numbers);
+}
+
+fn get_fuel(n: i32) -> i32 {
+    return (n as f32 / 3.0).floor() as i32 - 2;
 }
