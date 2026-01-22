4fa178f7a82cf0d36e81227d46e44b42ce73feb7
Danilo Petkovic <petkovicdanilo97@gmail.com>
2026-01-22 19:35:53 +0100

    2019 - day 6 part 2

--- 2019/aoc-2019/src/day6.rs
+++ 2019/aoc-2019/src/day6.rs
@@ -11,7 +11,8 @@ impl Day for Day6 {
         let orbit_map = parse_input(input_file)?;
         // println!("{orbit_map:#?}");
 
-        let orbit_count_map = calculate_orbits(String::from("COM"), &orbit_map)?;
+        let com = String::from("COM");
+        let orbit_count_map = calculate_orbits(&com, &orbit_map)?;
         // println!("{orbit_count_map:#?}");
 
         let mut res = 0;
@@ -24,31 +25,74 @@ impl Day for Day6 {
         return Ok(());
     }
 
-    fn part2(&mut self, _input_file: String) -> Result<()> {
-        todo!()
+    fn part2(&mut self, input_file: String) -> Result<()> {
+        let orbit_map = parse_input(input_file)?;
+
+        let mut parent_map = HashMap::<String, String>::new();
+
+        for (parent, children) in orbit_map {
+            for child in children {
+                parent_map.insert(child, parent.clone());
+            }
+        }
+
+        let you = String::from("YOU");
+        let san = String::from("SAN");
+
+        let you_path = find_path(&you, &parent_map);
+        let san_path = find_path(&san, &parent_map);
+
+        // println!("{you_path:#?}");
+        // println!("{san_path:#?}");
+
+        let mut idx = 0;
+        while you_path[idx] == san_path[idx] {
+            idx += 1;
+        }
+
+        // println!("idx={idx}");
+
+        let res = (you_path[idx..].len() - 1) + (san_path[idx..].len() - 1);
+        println!("{res}");
+
+        return Ok(());
     }
 }
 
 type OrbitMap = HashMap<String, Vec<String>>;
 
-fn calculate_orbits(start: String, orbit_map: &OrbitMap) -> Result<HashMap<String, u32>> {
+fn calculate_orbits(start: &String, orbit_map: &OrbitMap) -> Result<HashMap<String, u32>> {
     let mut count_map = HashMap::new();
-    calculate_orbits_inner(start, 0, orbit_map, &mut count_map)?;
+    calculate_orbits_inner(&start, 0, orbit_map, &mut count_map)?;
     return Ok(count_map);
 }
 
 fn calculate_orbits_inner(
-    start: String, count: u32, orbit_map: &OrbitMap, count_map: &mut HashMap<String, u32>
+    start: &String, count: u32, orbit_map: &OrbitMap, count_map: &mut HashMap<String, u32>
     ) -> Result<()> {
 
     count_map.insert(start.clone(), count);
-    if let Some(children) = orbit_map.get(&start) {
+    if let Some(children) = orbit_map.get(start) {
         for child in children {
-            calculate_orbits_inner(child.to_owned(), count + 1, orbit_map, count_map)?;
+            calculate_orbits_inner(&child, count + 1, orbit_map, count_map)?;
         }
     }
 
     return Ok(());
+}
+
+fn find_path(start: &str, parent_map: &HashMap<String, String>) -> Vec<String> {
+    let mut ret = Vec::new();
+
+    let mut curr = start;
+    while let Some(parent) = parent_map.get(curr) {
+        ret.push(curr.to_owned());
+        curr = parent;
+    }
+    ret.push(curr.to_owned());
+
+    ret.reverse();
+    return ret;
 }
 
 fn parse_input(input_file: String) -> Result<OrbitMap> {
